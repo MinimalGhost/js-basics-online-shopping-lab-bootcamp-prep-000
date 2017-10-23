f5fd370e04e4a48279a2ea7978cc3e254efef33f
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-23 19:23:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber) {
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
-  } else {
+  } else if (!cardNumber) {
     console.log('Sorry, we dont\'t have a credit card on file for you.')
   }
 }
